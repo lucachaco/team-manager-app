c79d8f5a9d2f876977c53e92a8fe8e90176118a0
Luis Chavarría <luiscarlosch@gmail.com>
2015-02-18 11:12:57 -0500

    Style nav.

--- public/js/team_manager_app_app.js
+++ public/js/team_manager_app_app.js
@@ -1,20 +1,20 @@
 'use strict';
-var teamManagerAppApp = angular.module('teamManagerAppApp',[]);
-teamManagerAppApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/team_manager_app/index', {templateUrl: '/team_manager_app/views/index', activeTab: 'Team managerApp', controller: 'IndexCtrl'});
-}]).run(['NavBarService', function(NavBarService) {
+var teamManagerAppApp = angular.module('teamManagerAppApp', []);
+teamManagerAppApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/team_manager_app/index', {
+        templateUrl: '/team_manager_app/views/index',
+        activeTab: 'Team managerApp',
+        controller: 'IndexCtrl'
+    });
+}]).run(['NavBarService', function (NavBarService) {
 // Register the main nav item, if any
-//    NavBarService.register({
-//        label: 'Team managerApp',
-//        url: '/team_manager_app/index',
-//        order: 100,
-//        showIf: function(CoreCompanyService, CoreUserService) {
-//            return true;
-//        },
-//        subNav: [
-//            {label: "Tab One", url: "/team_manager_app/tabone"},
-//            {label: "Tab Two", url: "/team_manager_app/tabtwo"}
-//        ]
-//    });
+    NavBarService.register({
+        label: 'Teams',
+        url: '/team_manager_app/index',
+        order: 100,
+        showIf: function (CoreCompanyService, CoreUserService) {
+            return true;
+        }
+    });
 
 }]);
